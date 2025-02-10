600506158dbe64e15d4a13f64b99f32022acf8e5
Mircea-Pavel ANTON <contact@mirceanton.com>
2025-02-11 00:00:27 +0200

    fix

--- provider.tf
+++ provider.tf
@@ -8,6 +8,6 @@ terraform {
 }
 
 provider "b2" {
-  application_key    = var.application_key
-  application_key_id = var.application_key_id
+  application_key    = var.backblaze_application_key
+  application_key_id = var.backblaze_application_key_id
 }
--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
-variable "application_key" {
+variable "backblaze_application_key" {
   type        = string
   sensitive   = true
   description = "B2 Application Key"
 }
 
-variable "application_key_id" {
+variable "backblaze_application_key_id" {
   type        = string
   sensitive   = true
   description = "B2 Application Key ID"
