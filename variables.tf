7b5f89463e5ad3658c8e9d2db175eea24c7f9c4f
Mircea-Pavel ANTON <contact@mirceanton.com>
2025-02-11 00:11:10 +0200

    feat: Initialize provider

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "backblaze_application_key" {
+  type        = string
+  sensitive   = true
+  description = "B2 Application Key"
+}
+
+variable "backblaze_application_key_id" {
+  type        = string
+  sensitive   = true
+  description = "B2 Application Key ID"
+}
